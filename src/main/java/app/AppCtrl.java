bd481ce7a826aa6f90c87ea1c79f818183df186a
Parth Patel <parth.p.188@gmail.com>
2017-06-18 02:31:06 -0400

    updated project structure

---
+++ src/main/java/app/AppCtrl.java
@@ -0,0 +1,7 @@
+package app;
+
+/**
+ * Created by Parth on 2017-06-18.
+ */
+public class AppCtrl {
+}
--- src/main/java/app/Grbk.java
+++ src/main/java/app/Grbk.java
@@ -3,17 +3,20 @@ package app;
 import com.mongodb.async.client.MongoClient;
 import com.mongodb.async.client.MongoClients;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 /**
  * Created by Parth on 2017-06-18.
  */
-public class Grbk extends Application{
+public class Grbk extends Application {
 
     @Override
-    public void start(Stage window) {
+    public void start(Stage window) throws Exception {
         window.setTitle("GRBK");
 
+        Parent root = FXMLLoader.load(getClass().getResource("/app.fxml"));
         MongoClient mongo = MongoClients.create();
         window.show();
     }
